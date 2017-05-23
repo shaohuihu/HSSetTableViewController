b3489b035672bd9f2a5e3ab23569840669996de1
hushaohui <hushaohui186@gmail.com>
2017-05-23 09:47:40 +0800

    升级版本到1.1.2

---
+++ HSSetTableViewCtrollerDemo/HSSetTableViewCtrollerDemo/Ctrollers/HSSetTableViewCustomController.h
@@ -0,0 +1,13 @@
+//
+//  HSSetTableViewCustomController.h
+//  HSSetTableViewCtrollerDemo
+//
+//  Created by hushaohui on 2017/5/23.
+//  Copyright © 2017年 ZLHD. All rights reserved.
+//
+
+#import "HSSetTableViewMainController.h"
+
+@interface HSSetTableViewCustomController : HSSetTableViewMainController
+
+@end
